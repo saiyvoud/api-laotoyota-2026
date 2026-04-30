0cb8d2c3109c9021d29cf2fb21f4e52345b9d609
winghor <jeethor9@gmail.com>
2026-05-01 00:08:22 +0700

    Success

--- src/config/db_mysql.js
+++ src/config/db_mysql.js
@@ -1,18 +1,18 @@
 import mysql from "mysql";
-const connected = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_laotoyota"
-});
-
 // const connected = mysql.createConnection({
-//   host: "54.255.223.5",
-//   port: 3306,
-//   user: "mysql",
-//   password: "Laotoyota123454321",
-//   database: "db_laotoyota"
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "db_laotoyota"
 // });
+
+const connected = mysql.createConnection({
+  host: "54.255.223.5",
+  port: 3306,
+  user: "mysql",
+  password: "Laotoyota123454321",
+  database: "db_laotoyota"
+});
 connected.connect((err) => {
   if (err) {
     console.error('❌ Failed Connect Database:', err.message);
