fbaa01f503fcc6fd7a601e4a8d6fd31f364a373e
winghor <jeethor9@gmail.com>
2026-06-23 19:48:29 +0700

    ppp

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 import express from "express";
 import './config/db_mysql.js';
+import cors from "cors";
+import router from "./router/index.js";
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser";
+import "./cron/deleteOldTimeFix.js";
 const app = express();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(cors());
 app.use(fileUpload())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "500mb", parameterLimit: 5000 }));
