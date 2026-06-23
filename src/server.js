adcdc8d2b93e39db8da9963a4a47fc7e77201087
winghor <jeethor9@gmail.com>
2026-06-23 19:35:50 +0700

    tt

--- src/server.js
+++ src/server.js
@@ -6,19 +6,7 @@ import fileUpload from "express-fileupload";
 import bodyParser from "body-parser";
 import "./cron/deleteOldTimeFix.js";
 const app = express();
-const allowedOrigins = ['https://webadmin.laotoyotaservice.la', 'http://localhost:5173'];
-
-app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true
-}));
+app.use(cors());
 app.use(fileUpload())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "500mb", parameterLimit: 5000 }));
