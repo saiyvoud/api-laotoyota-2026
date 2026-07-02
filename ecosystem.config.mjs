ab592381b1d1640bd1fc5fcdabaf83879ea66fc9
beesaiyvoud <saiyvouds@gmail.com>
2026-07-02 22:55:30 +0700

    add

---
+++ ecosystem.config.mjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "api-laotoyota-2026",
+      script: "./dist/main.js", // Path to your build output
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8000,
+        // Add your DB_URL or other secrets here
+      },
+      // Production Best Practices
+      max_memory_restart: "512M", // Restart if app leaks memory
+      autorestart: true,
+      watch: false,               // Disable for production
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      merge_logs: true
+    }
+  ]
+};
