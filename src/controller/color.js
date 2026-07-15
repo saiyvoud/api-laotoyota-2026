df606ba1dbf894f9a277b68a074e8c3bebb0a7d0
beesaiyvoud <saiyvouds@gmail.com>
2026-07-15 11:41:54 +0700

    add

--- src/controller/color.js
+++ src/controller/color.js
@@ -66,7 +66,7 @@ export default class ColorController {
            
             const data = await prisma.color.create({
                 data: {
-                    colorName, createBy: req.user
+                    colorName, 
                 }
             })
             return SendCreate(res, SMessage.Insert, data);
